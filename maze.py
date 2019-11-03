707501f9fc592bf01cd1e3ae7bd3a82d2741d7de
Mihai Ghidoveanu <mihai.ghidoveanu@gmail.com>
2019-11-03 18:48:20 +0200

    Add files via upload

--- maze.py
+++ maze.py
@@ -13,7 +13,7 @@ import collections
 #         self.is_start = is_start
 #         self.is_end = is_end
     
-Point = collections.namedtuple('Point', ['x', 'y'])
+Point = collections.namedtuple('Point', ['y', 'x'])
 
 def int_readline(f):
     return [int(number) for number in next(f).split()]
@@ -25,6 +25,8 @@ class Maze:
         assert(type(matrix[0]) is list)
         assert(type(start) is Point and type(end) is Point)
         self.matrix = matrix
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
         self.start = start
         self.end = end
 
@@ -54,33 +56,29 @@ class Maze:
             return Maze._read_matrix(fname)
 
     def _is_intersectionPoint(self, row, col):
-        rows = len(self.matrix)
-        cols = len(self.matrix[0])
-        points_on_col = [self.matrix[i][col] for i in [row + 1, row - 1]  if i in range(rows) ]
-        points_on_row = [self.matrix[row][j] for j in [col + 1, col - 1] if j in range(cols) ]
+        points_on_col = [self.matrix[i][col] for i in [row + 1, row - 1]  if i in range(self.rows) ]
+        points_on_row = [self.matrix[row][j] for j in [col + 1, col - 1] if j in range(self.cols) ]
         intersections = [ (p1 == 0 and p1 == p2) for p1 in points_on_col for p2 in points_on_row]
         is_intersection = sum(intersections) >= 1
         return is_intersection 
 
     def to_graph(self):
         graph = collections.defaultdict(list)
-        rows_size = len(self.matrix)
-        cols_size = len(self.matrix[0])
         # store last point encountered for each row and column
-        prev_on_col = [None] * cols_size  
-        prev_on_row = [None] *  rows_size 
+        prev_on_col = [None] * self.cols  
+        prev_on_row = [None] *  self.rows 
         for i, row in enumerate(self.matrix):
             for j, val in enumerate(row):
                 if val == 0 :
                     if self._is_intersectionPoint(i, j):
                         new_point = Point(i,j)
-                        # previous col index on row j
+                        # add path to previous col index on row j
                         prev_ci = prev_on_row[i] 
                         if prev_ci is not None:
                             old_point = Point(i, prev_ci)
                             graph[old_point].append(new_point)
                             graph[new_point].append(old_point)
-                        # previous row index on col i
+                        # add path to previous row index on col i
                         prev_ri = prev_on_col[j] 
                         if prev_ri is not None:
                             old_point = Point(prev_ri, j)
@@ -94,12 +92,27 @@ class Maze:
                     prev_on_row[i] = None                    
 
         return graph
+    
 
+import copy
 if __name__ == '__main__':
     fin = 'input_maze.txt'
     maze = Maze.from_file(fin)
     graph = maze.to_graph()
-    for key in graph.keys():
-        print( key, ' : ', graph[key])
+    m = copy.deepcopy(maze.matrix)
 
+    print(m)
 
+    for key in sorted(graph.keys()):
+        i, j = key
+        print(i, j)
+        m[i][j] = 5
+
+    with open('test.txt') as f:
+        for i, row in enumerate(m):
+            print(i)
+            print(m[i])
+            arr = next(f).split()
+            arr = [int(val) if val != 'x' else 5 for val in arr]
+            print(arr)
+            print()
