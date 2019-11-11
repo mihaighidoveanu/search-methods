65e14688c74074cf7dc290953afd0a9b038736a3
Mihai Ghidoveanu <mihai.ghidoveanu@gmail.com>
2019-11-11 08:36:15 +0200

    add dfs maze solver

--- maze.py
+++ maze.py
@@ -1,25 +1,56 @@
 import os
 import collections
 
-
-
-# class Point(object):
-
-#     def __init__(self, y, x, is_start = False, is_end = False):
-#         self.x = x
-#         self.y = y
-#         if is_start or is_end:
-#             assert(is_start != is_end)
-#         self.is_start = is_start
-#         self.is_end = is_end
-    
 Point = collections.namedtuple('Point', ['y', 'x'])
 
 def int_readline(f):
     return [int(number) for number in next(f).split()]
 
-class Maze:
+class Coords:
+    @staticmethod
+    def north():
+        return Point(-1,0)
 
+    @staticmethod 
+    def south():
+        return Point(1,0)
+
+    @staticmethod 
+    def east():
+        return Point(0,1)
+
+    @staticmethod  
+    def west():
+        return Point(0,-1)
+
+    @staticmethod   
+    def list():
+        return [Coords.north(), Coords.south(), Coords.east(), Coords.west()]
+	
+class State:
+    def __init__(self, point, orientation):
+        self.point = point
+        self.orientation = orientation
+
+    def next(self, orientation = None):
+        if orientation is None:
+            orientation = self.orientation
+        new_point = Point(self.point.y + orientation.y, self.point.x + orientation.x)
+        return State(new_point, orientation)
+
+    def neighbours(self):
+        nexts_or = Coords.list()
+        nexts_or.remove(self.orientation)
+        nexts = [self.next(orientation) for orientation in nexts_or]
+        return nexts
+
+    def __str__(self):
+        return f'{self.point}--->{self.orientation}'
+
+    def __repr__(self):
+        return str(self)
+
+class Maze:
     def __init__(self, matrix, start, end):
         assert(type(matrix is list))
         assert(type(matrix[0]) is list)
@@ -27,15 +58,36 @@ class Maze:
         self.matrix = matrix
         self.rows = len(matrix)
         self.cols = len(matrix[0])
-        self.start = start
+        self.start = State(start, Coords.north())
         self.end = end
 
-    @staticmethod
-    def _read_graph(fname):
-        pass
+    def __getitem__(self, point):
+        i, j = point
+        if i >= self.rows or i < 0 :
+            return None
+        if j >= self.cols or j < 0 :
+            return None
+        return self.matrix[i][j]
+
+    def __setitem__(self, point, value):
+        i, j = point
+        if i >= self.rows or i < 0 :
+            return 
+        if j >= self.cols or j < 0 :
+            return 
+        self.matrix[i][j] = value
+
+    def is_finish(self, point):
+        return point == self.end 
+
+    def can_visit(self, point):
+        return self[point] == 0
+
+    def visit(self, point):
+        self[point] = 2
 
     @staticmethod
-    def _read_matrix(fname):
+    def read_matrix(fname):
         matrix = []
         with open(fname, 'r') as f:
             rows = int_readline(f)[0]
@@ -48,71 +100,8 @@ class Maze:
         maze = Maze(matrix, start, end)
         return maze
 
-    @staticmethod
-    def from_file(fname, graph_format = False):
-        if graph_format:
-            return Maze._read_graph(fname)
-        else:
-            return Maze._read_matrix(fname)
-
-    def _is_intersectionPoint(self, row, col):
-        points_on_col = [self.matrix[i][col] for i in [row + 1, row - 1]  if i in range(self.rows) ]
-        points_on_row = [self.matrix[row][j] for j in [col + 1, col - 1] if j in range(self.cols) ]
-        intersections = [ (p1 == 0 and p1 == p2) for p1 in points_on_col for p2 in points_on_row]
-        is_intersection = sum(intersections) >= 1
-        return is_intersection 
-
-    def to_graph(self):
-        graph = collections.defaultdict(list)
-        # store last point encountered for each row and column
-        prev_on_col = [None] * self.cols  
-        prev_on_row = [None] *  self.rows 
-        for i, row in enumerate(self.matrix):
-            for j, val in enumerate(row):
-                if val == 0 :
-                    if self._is_intersectionPoint(i, j):
-                        new_point = Point(i,j)
-                        # add path to previous col index on row j
-                        prev_ci = prev_on_row[i] 
-                        if prev_ci is not None:
-                            old_point = Point(i, prev_ci)
-                            graph[old_point].append(new_point)
-                            graph[new_point].append(old_point)
-                        # add path to previous row index on col i
-                        prev_ri = prev_on_col[j] 
-                        if prev_ri is not None:
-                            old_point = Point(prev_ri, j)
-                            graph[old_point].append(new_point)
-                            graph[new_point].append(old_point)
-                        # update intersections
-                        prev_on_col[j] = i
-                        prev_on_row[i] = j
-                elif val == 1 :
-                    prev_on_col[j] = None
-                    prev_on_row[i] = None                    
-
-        return graph
-    
-
 import copy
 if __name__ == '__main__':
     fin = 'input_maze.txt'
-    maze = Maze.from_file(fin)
-    graph = maze.to_graph()
-    m = copy.deepcopy(maze.matrix)
+    maze = Maze.read_matrix(fin) 
 
-    print(m)
-
-    for key in sorted(graph.keys()):
-        i, j = key
-        print(i, j)
-        m[i][j] = 5
-
-    with open('test.txt') as f:
-        for i, row in enumerate(m):
-            print(i)
-            print(m[i])
-            arr = next(f).split()
-            arr = [int(val) if val != 'x' else 5 for val in arr]
-            print(arr)
-            print()
---
+++ search.py
@@ -0,0 +1,50 @@
+import os
+import collections
+import copy
+
+def dfs(maze, state):
+    maze = copy.deepcopy(maze)
+    stack = [state]
+    path = [state]
+
+    while len(stack) != 0 :
+        state = stack.pop(-1) 
+        point = state.point
+
+        while maze.can_visit(point):
+            # print('Forward ', point, maze[point], state.orientation)
+            if maze.is_finish(point):
+                print('FINISH')
+                print('Traversed matrix')
+                for row in maze.matrix:
+                    for val in row:
+                        print(f'{val:d}   ', end = '')
+                    print()
+                return point 
+            maze.visit(point)
+            neighbours = state.neighbours()
+            # print('Neighbors', neighbours)
+            for  nstate in neighbours:
+                if maze.can_visit(nstate.point):
+                    # print('Add Neighbour ', nstate)
+                    stack.append(nstate)
+            state = state.next()
+            point = state.point
+
+
+def bfs():
+    pass
+
+from maze import Maze, State
+
+if __name__ == '__main__':
+    fin = 'input_maze.txt'
+    maze = Maze.read_matrix(fin) 
+
+    for row in maze.matrix:
+        print(row)
+
+    print('The end should be ', maze.end)
+
+    print('dfs ', dfs(maze, maze.start))
+
