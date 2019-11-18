7bb05bbb71c490eac0584824a62267dee0ca16bf
Mihai Ghidoveanu <mihai.ghidoveanu@gmail.com>
2019-11-18 23:09:21 +0200

    add evaluation method for path

--- informed/search.py
+++ informed/search.py
@@ -29,33 +29,44 @@ class PathFinder(AStar):
 
 def best_first_search(graph, start, end):
     pq = [] # priority queue
+    path = []
     visited = set() 
     heapq.heappush(pq, (0, start))
     while len(pq) != 0:
         _, point = heapq.heappop(pq)
-        print(point)
+        path.append(point)
         if point == end:
             print('Finished')
-            return None
+            return path
         else:
             for neighbor in graph[point]:
                 if neighbor not in visited:
                     visited.add(neighbor)
                     dist = np.linalg.norm( neighbor - end)
                     heapq.heappush(pq, (dist, neighbor))
-    print('Not found')
+    return path
 
-    
+def eval(path):
+    d = 0
+    for i in range(len(path) - 1):
+        d += np.linalg.norm( path[i + 1] - path[i])
+    return d
 
-
-if __name__ == '__main__':
-    fin = 'input.txt'
+def run(fin):
     map = Map.from_file(fin)
     solver = PathFinder(map.graph)
     solution = solver.astar(map.start, map.end)
     if solution is not None:
         print(f'Shortest path from {map.start} to {map.end} :')
-        print(list(solution))
+        path = list(solution)
+        print(path)
+        print('Cost ', eval(path))
     else:
         print('No solution found')
-    best_first_search(map.graph, map.start, map.end)
+    path = best_first_search(map.graph, map.start, map.end)
+    print(path)
+    print('Cost ', eval(path))
+
+import fire
+if __name__ == '__main__':
+    fire.Fire(run)
